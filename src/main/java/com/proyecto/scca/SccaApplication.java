ddcc3ae29fec42bdfdb32fb9148840b58630265c
HoldenAndy <kaziroz09@gmail.com>
2026-05-10 21:18:45 -0500

    feat: Creacion de modelos (DTOs y Entities)

---
+++ src/main/java/com/proyecto/scca/SccaApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.scca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SccaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SccaApplication.class, args);
+	}
+
+}
